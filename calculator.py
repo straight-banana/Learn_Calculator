1c4c527f611bfc4c8c8ea9d66e6623c4065ae50d
Abdus Salam Tasnim <151547086+straight-banana@users.noreply.github.com>
2024-05-20 22:31:05 +0600

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,20 @@
+x = float(input()) 
+m = input()
+y = float(input())
+
+if m == "+" :
+    z = x + y
+
+if m == "-" :
+    z = x - y
+
+if m == "*" :
+    z = x * y
+
+if m == "/" :
+    z = x / y
+
+if m == "^" :
+    z = x ** y
+
+print(f"{z}")
