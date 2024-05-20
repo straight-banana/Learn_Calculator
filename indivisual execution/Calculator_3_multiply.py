4a027862e34a18239450238bf8a57470c8e5f1a1
Abdus Salam Tasnim <151547086+straight-banana@users.noreply.github.com>
2024-05-20 22:33:54 +0600

    Add files via upload

---
+++ indivisual execution/Calculator_3_multiply.py
@@ -0,0 +1,6 @@
+x = float(input("value: ")) #int can be used to make it simpler
+y = float(input("Value: "))
+
+z = round(x * y , 2) #z = x * y (if you use int)
+
+print(f"{z:,}")
---
+++ indivisual execution/Calculator_4_division.py
@@ -0,0 +1,6 @@
+x = float(input("value: ")) #using int will not produce actual value everytime
+y = float(input("value: "))
+
+z = round(x / y , 2) 
+
+print(f"{z:,}")
